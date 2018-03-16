e1624da96d520002df4985e98d835486fb61280b
David Ferreira De Menezes Lyra <david.menezes@oi.net.br>
2018-03-16 14:52:22 -0300

    Studying Data Structure

--- src/main/java/com/java8/examples/SetApplication.java
+++ src/main/java/com/java8/examples/SetApplication.java
@@ -1,7 +1,12 @@
 package com.java8.examples;
 
+import com.java8.examples.set.Conjunto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashSet;
+import java.util.Set;
+
 
 @SpringBootApplication
 public class SetApplication {
@@ -9,6 +14,31 @@ public class SetApplication {
     public static void main(String[] args) {
         SpringApplication.run(SetApplication.class, args);
 
+        Conjunto conjunto = new Conjunto();
+
+        conjunto.adiciona("Mauricio");
+        System.out.println(conjunto);
+
+        conjunto.adiciona("Mauricio");
+        System.out.println(conjunto);
+
+        conjunto.adiciona("Marcelo");
+        conjunto.adiciona("Guilherme");
+        System.out.println(conjunto);
+
+        conjunto.remove("Mauricio");
+        System.out.println(conjunto);
+
+        // À estrutura de Conjuntos damos o nome de Set e sua implementação é o HashSet:
+
+        Set<String> conjuntoDoJava = new HashSet<String>();
+
+        conjuntoDoJava.add("Mauricio");
+
+        // Para espalhar os dados, o HashSet se utiliza do método HashCode
+
+        String x = "Guilherme";
+        x.hashCode();
 
     }
 
--- src/main/java/com/java8/examples/set/Set.java
+++
@@ -1,4 +0,0 @@
-package com.java8.examples.set;
-
-public class Set {
-}
